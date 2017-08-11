cbfb8a62992531d5301348310b8690960bf3c727
Justin Kim <jjkim928@gmail.com>
2017-08-11 18:59:52 -0400

    YES! got the search termURL to build properly

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("Talking...");
+
 let url = "https://itunes.apple.com/search?term="
 let limit = "&limit=20";
 let entity = "&entity=music"
@@ -5,7 +7,13 @@ let entity = "&entity=music"
 let searchBar = document.getElementById("searchBar");
 let btnSearch = document.getElementById("btnSearch");
 
-function search() {
+let formSubmit = document.getElementById("formSubmit");
+
+formSubmit.addEventListener("submit", callback, false);
+
+function callback(e) {
+	e.preventDefault();
 	let user_input = searchBar.value;
 	let search_term = `${url}${user_input}${limit}${entity}`;
+	console.log(search_term);
 }
